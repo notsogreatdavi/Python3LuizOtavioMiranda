ba7cf7575826592d3a698369b0f2ce6dc445a8a4
Davi Vieira <davivdsilva03@gmail.com>
2025-02-20 10:46:08 -0300

    Primeira aula sobre comentários e docstrings

---
+++ aula1.py
@@ -0,0 +1,13 @@
+# Escrevendo um comentário simples
+print( "Hello World!" )
+
+# Docstrings como comentários para fazer comentários docstrings
+
+"""
+DocString
+
+O interpretador do python lê isso como uma string
+E escrever o que eu quiser, é possível
+Pular linhas depois
+Testando
+"""
